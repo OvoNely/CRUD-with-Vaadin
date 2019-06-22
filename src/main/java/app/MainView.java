c8a87a045b509d6109e1a658307c175e4b91d1dd
OvoNely <lisper@mai.ru>
2019-06-22 18:52:25 +0700

    изменение MainView.java: добавление текстового поля для фильтрации данных

--- src/main/java/app/MainView.java
+++ src/main/java/app/MainView.java
@@ -4,7 +4,10 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import org.springframework.util.StringUtils;
 
 @Route
 public class MainView extends VerticalLayout{
@@ -16,10 +19,21 @@ public class MainView extends VerticalLayout{
         this.repo = repo;
         this.grid = new Grid<>(Customer.class);
         add(grid);
-        listCustomers();
+
+        TextField filter = new TextField();
+        filter.setPlaceholder("Filter by last name");
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+        add(filter, grid);
+
+        listCustomers(null);
     }
 
-    private void listCustomers() {
-        grid.setItems(repo.findAll());
+    private void listCustomers(String filterText) {
+        if (StringUtils.isEmpty(filterText)) {
+            grid.setItems(repo.findAll());
+        } else {
+            grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
+        }
     }
 }
